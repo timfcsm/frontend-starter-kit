0425d903fc86542032003f16b45c8134be749ed5
Timur Makarchenko <timur@100up.ru>
2017-06-27 12:53:24 +0300

    svg

--- gulpfile.js/tasks/svg.js
+++ gulpfile.js/tasks/svg.js
@@ -1,18 +1,18 @@
-const config = require('../config');
-const gulp = require('gulp');
+const config   = require('../config');
+const gulp     = require('gulp');
 const svgstore = require('gulp-svgstore');
-const svgmin = require('gulp-svgmin');
-const path = require('path');
-const size = require('gulp-size');
-const rename = require('gulp-rename');
+const svgmin   = require('gulp-svgmin');
+const path     = require('path');
+const size     = require('gulp-size');
+const rename   = require('gulp-rename');
 
-gulp.task('svg', function() {
+gulp.task('svg', function () {
   return gulp
     .src(path.join(config.paths.svg, '**', '*.svg'))
-    .pipe(rename({ prefix: 'i-' }))
+    .pipe(rename({prefix: 'i-'}))
     .pipe(svgmin())
     .pipe(svgstore())
-    .pipe(rename('svg.html'))
-    .pipe(size({ title: 'SVG', gzip: config.prod }))
+    .pipe(rename('sprite.svg'))
+    .pipe(size({title: 'SVG', gzip: config.prod}))
     .pipe(gulp.dest(config.paths.views));
 });
